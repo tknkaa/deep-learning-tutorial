cebf09129a658583aa3247ebe9f0f5699919ad84
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-03-21 14:36:52 +0900

    normalize and one-hot encoding

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import matplotlib.pyplot as plt
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-plt.imshow(x_train[0])
-plt.savefig("out.png")
+x_train, x_test = x_train.astype("float32") / 255, x_test.astype("float32") / 255
+y_train, y_test = tf.keras.utils.to_categorical(y_train), tf.keras.utils.to_categorical(
+    y_test
+)
