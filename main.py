473a20886476faddab7087bec02809db3321a72f
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-03-21 14:13:01 +0900

    show image from mnist

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-import numpy as np
-import matplotlib.pylab as plt
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-def step_function(x):
-    return np.array(x > 0, dtype=int)
-
-
-x = np.arange(-5.0, 5.0, 0.1)
-y = step_function(x)
-plt.plot(x, y, label="step")
-plt.ylim(-0.1, 1.1)
-
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-
-x = np.arange(-5.0, 5.0, 0.1)
-y = sigmoid(x)
-plt.plot(x, y, label="sigmoid")
-plt.legend()
+plt.imshow(x_train[0])
 plt.savefig("out.png")
